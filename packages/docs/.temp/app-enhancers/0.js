42e77548084744e2eb72def4a8b4d612dc089cda
ULIVZ <472590061@qq.com>
2018-11-22 17:58:29 +0800

    chore: remove temp files

--- packages/docs/.temp/app-enhancers/0.js
+++
@@ -1,2 +0,0 @@
-import "/Users/ulivz/Documents/ulivz/__ulivz__/sherry/node_modules/@vuepress/core/lib/internal-plugins/style/client.js"
-export default {}
--- packages/docs/.temp/internal/app-enhancers.js
+++
@@ -1,9 +0,0 @@
-import m0 from "/Users/ulivz/Documents/ulivz/__ulivz__/sherry/packages/docs/.temp/app-enhancers/0.js"
-import m1 from "/Users/ulivz/Documents/ulivz/__ulivz__/sherry/packages/docs/.temp/app-enhancers/data-block.js"
-import m2 from "/Users/ulivz/Documents/ulivz/__ulivz__/sherry/packages/docs/.temp/app-enhancers/global-components-17.js"
-
-export default [
-  m0,
-  m1,
-  m2
-]
